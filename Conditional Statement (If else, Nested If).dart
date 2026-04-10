23489643f4a2a4e4c2f87f048f7dfbdf67838489
Renuvarma <160472711+Renuvarma@users.noreply.github.com>
2026-04-10 15:45:11 +0530

    Create Conditional Statement (If else, Nested If).dart

---
+++ Conditional Statement (If else, Nested If).dart
@@ -0,0 +1,38 @@
+//Conditional Statement
+
+
+
+//If-else Statement
+
+void main(){
+  
+  int salary=20000;
+  
+  if(salary>=30000){
+    print("Great...You are doing Good job..!!");
+  }
+  else{
+    print("Try to get more Salary....");
+  }
+  
+  
+  
+//Nested If statement
+  
+  int score=20;
+  
+  if(score>=90){
+    print("well done");
+  }
+  else if(score>=60){
+    print("Good");
+  }
+  else if(score>=30){
+    print("work hard");
+  }
+  else{
+    print("failed..Better luck next time");
+  }
+  
+}
+
