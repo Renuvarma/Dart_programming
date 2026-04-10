532facbf168e4bb8a558c3c7be8c2f022678c24a
Renuvarma <160472711+Renuvarma@users.noreply.github.com>
2026-04-10 12:28:28 +0530

    Create Data Types.dart

---
+++ Data Types.dart
@@ -0,0 +1,25 @@
+/*Data Types(in this dart language before assigning a value we have to mention 
+its data type or else we can also put 'var' keyword)*/
+
+void main(){
+  //Integer(means without decimal values)
+  int a=10;
+  print(a);
+  
+  //Double(means with decimal values)
+  double b=100.565669;
+  print(b);
+  
+  //String(alphabets or characters)
+  String name='renu';
+  var lastname="varma";
+  print(name);
+  print(lastname);
+  
+  //boolean(true or false)
+  bool alive=true;
+  bool dead=false;
+  print(alive);
+  print(dead);
+  
+}
