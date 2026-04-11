a4cae4eac0d36a4cb38103d95177b520cbb7ff3c
Renuvarma <160472711+Renuvarma@users.noreply.github.com>
2026-04-11 10:09:16 +0530

    Create Switch Case Statements.dart

---
+++ Switch Case Statements.dart
@@ -0,0 +1,24 @@
+//Switch Statement (Instead of writing many if-else conditions)
+
+void main(){
+  
+  var grade='k';
+  
+  switch(grade){
+      
+    case 'A':
+      print("Excellent");
+      break;
+    case 'B':
+      print("good");
+      break;
+    case 'C':
+      print("Try hard");
+      break;
+    default:
+      print("Better luck next time");
+      break;
+      
+  }
+  
+}
