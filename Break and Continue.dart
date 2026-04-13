e060dbe0e61772373d093a227c23664234c15652
Renuvarma <160472711+Renuvarma@users.noreply.github.com>
2026-04-13 10:48:01 +0530

    Create Break and Continue.dart

---
+++ Break and Continue.dart
@@ -0,0 +1,27 @@
+//Break And Continue Statements
+  
+//Break Statement  stop the loop immediately.
+  
+ void main(){
+   for(var i =0;i<=20;i++){
+     if(i ==15){
+       break;
+     }
+    
+     print(i);
+   }
+ } 
+  
+
+
+//Continue Statement  skip the current iteration and jump to the next iteration.
+
+void main(){
+   for(var i =0;i<=20;i++){
+     if(i ==15){
+       continue;
+     }
+    
+     print(i);
+   }
+ } 
